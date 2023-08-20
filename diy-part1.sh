4e611ff943d9f24a48e3186c45846ac945059a5f
smdx <smdx02@gmail.com>
2023-08-20 15:04:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,6 @@
 
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci-smartdns-new-version' >>feeds.conf.default
+echo 'src-git smartdns https://github.com/pymumu/smartdns' >>feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
