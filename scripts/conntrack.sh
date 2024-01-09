2ca7d7ffe249f1d7770945b07b031f4be116bc7c
MenGQiu <smdx02@gmail.com>
2024-01-09 12:48:04 +0800

    Update conntrack.sh

--- scripts/conntrack.sh
+++ scripts/conntrack.sh
@@ -1,4 +1,4 @@
-cat > /files/etc/uci-defaults/zzz-conntrack << "EOF"
+cat > openwrt/files/etc/uci-defaults/zzz-conntrack << "EOF"
 #!/bin/sh
   
 sed -i 's/net.nf_conntrack_max/net.netfilter.nf_conntrack_max/g' /usr/lib/lua/luci/view/admin_status/index.htm
