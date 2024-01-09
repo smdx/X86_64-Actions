7c7217229b9d47b90c92113b80a7f6f5b5e513fc
MenGQiu <smdx02@gmail.com>
2024-01-09 13:01:36 +0800

    Update conntrack.sh

--- scripts/conntrack.sh
+++ scripts/conntrack.sh
@@ -1,4 +1,4 @@
-cat > $GITHUB_WORKSPACE/openwrt/files/etc/uci-defaults/zzz-conntrack << "EOF"
+cat > $GITHUB_WORKSPACE/openwrt/package/base-files/files/etc/uci-defaults/zzz-conntrack << "EOF"
 #!/bin/sh
   
 sed -i 's/net.nf_conntrack_max/net.netfilter.nf_conntrack_max/g' /usr/lib/lua/luci/view/admin_status/index.htm
