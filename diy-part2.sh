ff2ce0e3364c0396ec6edc4af08069e341bbadf8
MenGQiu <smdx02@gmail.com>
2024-01-19 13:16:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -35,6 +35,9 @@ sed -i 's/\[ -n "$(command -v ip6tables)" \] && ip6tables -t nat -A PREROUTING -
 #在防火墙自定义添加FTP规则 9099端口对应FTP转发端口
 sed -i '46a echo '\''iptables -t raw -A PREROUTING -p tcp --dport 9099 -j CT --helper ftp'\'' >> /etc/firewall.user\n' package/lean/default-settings/files/zzz-default-settings
 
+#防止init.d目录下权限问题
+sed -i '60a\\nchmod 0755 /etc/init.d/*'  /root/zzz-default-settings.txt
+
 #nlbwmon 修复log警报
 sed -i '/customized in this file/a net.core.wmem_max=16777216' package/base-files/files/etc/sysctl.conf
 sed -i '/customized in this file/a net.core.rmem_max=16777216' package/base-files/files/etc/sysctl.conf
