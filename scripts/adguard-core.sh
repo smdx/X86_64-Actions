2f0d5a38f9998153854f9b34c115de0422cadabf
MenGQiu <smdx02@gmail.com>
2024-01-20 15:26:47 +0800

    Update adguard-core.sh

--- scripts/adguard-core.sh
+++ scripts/adguard-core.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir -p $GITHUB_WORKSPACE/openwrt/files/usr/bin/AdGuardHome
+mkdir -p $GITHUB_WORKSPACE/files/usr/bin/AdGuardHome
 
 AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
 
-wget -qO- $AGH_CORE | tar xOvz > $GITHUB_WORKSPACE/openwrt/files/usr/bin/AdGuardHome/AdGuardHome
+wget -qO- $AGH_CORE | tar xOvz > $GITHUB_WORKSPACE/files/usr/bin/AdGuardHome/AdGuardHome
 
-chmod +x $GITHUB_WORKSPACE/openwrt/files/usr/bin/AdGuardHome/AdGuardHome
+chmod +x $GITHUB_WORKSPACE/files/usr/bin/AdGuardHome/AdGuardHome
