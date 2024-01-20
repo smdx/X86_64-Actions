ad250a25da8b93d661a68b9a37f3fe0856488522
MenGQiu <smdx02@gmail.com>
2024-01-20 11:48:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -43,7 +43,7 @@ sed -i '/customized in this file/a net.core.wmem_max=16777216' package/base-file
 sed -i '/customized in this file/a net.core.rmem_max=16777216' package/base-files/files/etc/sysctl.conf
 
 # 移除要替换的包
-rm -rf feeds/packages/net/smartdns
+#rm -rf feeds/packages/net/smartdns
 rm -rf feeds/smpackage/smartdns
 rm -rf feeds/smpackage/luci-app-smartdns
 rm -rf feeds/smpackage/adguardhome
@@ -94,8 +94,8 @@ function git_sparse_clone() {
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 #
 # Smartdns
-git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
+#git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+#git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
 #
 # AdguardHome
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
