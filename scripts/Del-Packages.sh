23859429ba0231a5b2f92de703704ff55505091b
MenGQiu <smdx02@gmail.com>
2024-01-22 15:28:37 +0800

    Update Del-Packages.sh

--- scripts/Del-Packages.sh
+++ scripts/Del-Packages.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # 移除要替换的包
-#rm -rf feeds/packages/net/smartdns
-rm -rf feeds/smpackage/smartdns
-rm -rf feeds/smpackage/luci-app-smartdns
+rm -rf feeds/packages/net/smartdns
+#rm -rf feeds/smpackage/smartdns
+#rm -rf feeds/smpackage/luci-app-smartdns
 rm -rf feeds/smpackage/adguardhome
 rm -rf feeds/smpackage/luci-app-adguardhome
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
