6f38a3e1210d7e42909ae8affa95a3c099ef06ec
MenGQiu <smdx02@gmail.com>
2024-02-09 15:39:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -71,7 +71,7 @@ sed -i '/customized in this file/a net.core.rmem_max=16777216' package/base-file
 #sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=8.5.0/g' feeds/packages/net/curl/Makefile
 #sed -i 's/PKG_HASH:=.*/PKG_HASH:=ce4b6a6655431147624aaf582632a36fe1ade262d5fab385c60f78942dd8d87b/g' feeds/packages/net/curl/Makefile
 #sed -i 's/PKG_RELEASE:=.*/PKG_RELEASE:=1/g' feeds/packages/net/curl/Makefile
-pushd feeds/packages/net/curl
+pushd feeds/packages/net/
 rm -fr curl && svn co https://github.com/coolsnowwolf/packages/tree/986bdbcc1a0773434f04339d5ddee79da39fd948/net/curl
 
 # 添加额外非必须软件包
