aaa1ecfcbc42e990401626d147f83b96b9c34980
MenGQiugit config --global user.namegit config --global user.email smdx02@gmail.comgit config --global user.email <smdx02@gmail.com>
2024-11-23 18:27:49 +0800

    更新 PKG_Version_Get.sh

--- scripts/PKG_Version_Get.sh
+++ scripts/PKG_Version_Get.sh
@@ -2,7 +2,9 @@
 # Package  Version Output to Env
 adguardhome_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/adguardhome/Makefile)
 mosdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' package/mosdns/mosdns/Makefile)
-smartdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/luci-app-smartdns/Makefile)
+mosdns_release=$(grep -oP 'PKG_RELEASE:=\K[^ ]+' package/mosdns/mosdns/Makefile)
+smartdns_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/smartdns/Makefile)
+smartdns_release=$(grep -oP 'PKG_RELEASE:=\K[^ ]+' feeds/kenzo/smartdns/Makefile)
 passwall_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/luci-app-passwall/Makefile)
 passwall_release=$(grep -oP 'PKG_RELEASE:=\K[^ ]+' feeds/small/luci-app-passwall/Makefile)
 passwall2_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/luci-app-passwall2/Makefile)
@@ -10,8 +12,8 @@ passwall2_release=$(grep -oP 'PKG_RELEASE:=\K[^ ]+' feeds/small/luci-app-passwal
 openclash_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/small/luci-app-openclash/Makefile)
 store_version=$(grep -oP 'PKG_VERSION:=\K[^ ]+' feeds/kenzo/luci-app-store/Makefile)
 echo "adguardhome=$adguardhome_version" >> $GITHUB_ENV
-echo "mosdns=$mosdns_version" >> $GITHUB_ENV
-echo "smartdns=$smartdns_version" >> $GITHUB_ENV
+echo "mosdns=${mosdns_version}-${mosdns_release}" >> $GITHUB_ENV
+echo "smartdns=Release ${smartdns_version}-${smartdns_release}" >> $GITHUB_ENV
 echo "passwall_version=${passwall_version}-${passwall_release}" >> $GITHUB_ENV
 echo "passwall2_version=${passwall2_version}-${passwall_release}" >> $GITHUB_ENV
 echo "openclash=$openclash_version" >> $GITHUB_ENV
